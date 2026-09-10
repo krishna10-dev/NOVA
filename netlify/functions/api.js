7c12913dac43685c5b9d0c8b8aa994d5408397d9
krishna10-dev <kk1930778@gmail.com>
2026-09-10 16:31:47 +0530

    update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: require("path").resolve(__dirname, "../../server/.env")
+});
+
 const serverless = require("serverless-http");
 
 const app = require("../../server/app");
