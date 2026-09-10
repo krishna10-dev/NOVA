f563cfdbf330c791c8d63c019eac9e50bb3a914e
krishna10-dev <kk1930778@gmail.com>
2026-09-10 16:37:22 +0530

    Fix: use nft bundler and include server files for Netlify function

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,9 +1,4 @@
-require("dotenv").config({
-  path: require("path").resolve(__dirname, "../../server/.env")
-});
-
 const serverless = require("serverless-http");
-
 const app = require("../../server/app");
 
 module.exports.handler = serverless(app);
